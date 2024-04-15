74b995927e1f974376756ae701704cceafc3aba8
Manaway <tomasm39@outlook.com>
2024-04-14 23:43:58 -0400

    update config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -4,7 +4,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
   title: 'UniverseNodes',
-  tagline: 'Dinosaurs are cooool',
+  tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
